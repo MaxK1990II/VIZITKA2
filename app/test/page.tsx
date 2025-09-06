4d8306d92e48588909a4ec7cf059c9b2965258ba
СтройЛогистика Developer <developer@stroy-logistika.com>
2025-09-07 00:25:06 +0300

    test: add diagnostic test page

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontSize: '24px', color: 'white' }}>
+      <h1>Тестовая страница работает!</h1>
+      <p>Если вы видите это, значит, деплой на Vercel функционирует правильно.</p>
+    </div>
+  );
+}
