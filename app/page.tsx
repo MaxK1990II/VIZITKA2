122cf7c0987cabbcd7b0646495d2d7df3937f537
СтройЛогистика Developer <developer@stroy-logistika.com>
2025-09-09 00:14:20 +0300

    chore(debug): disable ScrollAnim again to isolate interaction bug

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       {/* <UniverseBackground /> */}
       <main>
         <Loader />
-        <ScrollAnim />
+        {/* <ScrollAnim /> */}
         <Hero3D />
         <About />
         <Timeline />
