e358b75058bc45b7763f799f2be05392d9309820
СтройЛогистика Developer <developer@stroy-logistika.com>
2025-09-09 00:08:52 +0300

    chore(debug): disable UniverseBackground on provider-only page

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <SmoothScrollProvider>
       <CustomCursor />
-      <UniverseBackground />
+      {/* <UniverseBackground /> */}
       <main>
         {/* <Loader /> */}
         {/* <ScrollAnim /> */}
